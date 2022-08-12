8da253bc4356099bb0cd979366c41eabe2895f76
Lupen07 <110674819+Lupen07@users.noreply.github.com>
2022-08-12 12:28:22 +0300

    Update options.py

--- options.py
+++ options.py
@@ -1,10 +1,1 @@
-name = input('Please ente your name ? :')
-age = input('Please ente your age ? :')
-
-age = int(age)
-if age >= 18:
-    print("You can enter, because your age is 18 or more that 18")
-else:
-    print("You can't enter, because your age is less")
-
-print("your name :", name, "your age :", str(age));
+print("THIS IS ALL");
