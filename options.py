b7315a6e08be30f5de28d46c3db8e31aa1979a1c
Lupen07 <nazario.lupen@gmail.com>
2022-08-12 12:25:01 +0300

    third

--- options.py
+++ options.py
@@ -1,5 +1,10 @@
-x = 25
-y = 40
+name = input('Please ente your name ? :')
+age = input('Please ente your age ? :')
 
-equal = x * y
-print("the number :" + x + " * " + y + " = " + equal);
+age = int(age)
+if age >= 18:
+    print("You can enter, because your age is 18 or more that 18")
+else:
+    print("You can't enter, because your age is less")
+
+print("your name :", name, "your age :", str(age));
